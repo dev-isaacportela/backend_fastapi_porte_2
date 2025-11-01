c5ccaa24c52b47cd5a4c982324de00c48445cf32
isaac <isaacportela0903@gmail.com>
2025-10-31 23:39:25 -0300

    Feat: Core 2

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,9 @@
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY: str = os.getenv("SECRET_KEY", "VALOR_PADRAO_DE_SEGURANCA") 
+ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+SQLALCHEMY_DATABASE_URL: str = "sqlite:///./banco.db"
