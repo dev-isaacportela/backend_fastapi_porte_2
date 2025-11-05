b717b25f3909502284652c25cb72ebf531d66162
isaac <isaacportela0903@gmail.com>
2025-11-05 19:52:58 -0300

    Feat: .gitignore

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.api.v1.endpoints import auth
 from app.db.database import Base, engine 
 
+#uvicorn app.main:app --reload
+
 app = FastAPI(
     title="API de portes de arma em FastAPI",
     description="Autenticação JWT/Bcrypt."
